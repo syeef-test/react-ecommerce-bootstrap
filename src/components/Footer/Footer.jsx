5a583336890885fe62ccb3c2e6c13290e13eebe5
syeef <syeefislam@yahoo.com>
2024-02-05 11:01:01 +0530

    home page details added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,26 +1,29 @@
 import React from "react";
+import classes from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className="bg-info text-white">
-      <div className="d-flex justify-content-around align-items-center">
-        <h1>The Generics</h1>
-        <div className="d-flex justify-content-around">
-          <a href="https://www.youtube.com">
-            <img
-              src="./img/6260efc8fc9a9002669d2f4ad9956cc0.jpg"
-              alt="YouTube Logo"
-            />
-          </a>
-          <a href="https://spotify.com">
-            <img src="./img/Spotify Logo.png" alt="Spotify Logo" />
-          </a>
-          <a href="https://facebook.com">
-            <img src="./img/Facebook Logo.png" alt="Facebook Logo" />
-          </a>
+    <section className={classes.fixedfooter}>
+      <footer className="bg-info text-white">
+        <div className="d-flex justify-content-around align-items-center">
+          <h1>The Generics</h1>
+          <div className="d-flex justify-content-around">
+            <a href="https://www.youtube.com">
+              <img
+                src="./img/6260efc8fc9a9002669d2f4ad9956cc0.jpg"
+                alt="YouTube Logo"
+              />
+            </a>
+            <a href="https://spotify.com">
+              <img src="./img/Spotify Logo.png" alt="Spotify Logo" />
+            </a>
+            <a href="https://facebook.com">
+              <img src="./img/Facebook Logo.png" alt="Facebook Logo" />
+            </a>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </section>
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div className="bg-light py-4 text-center">
-      <h1>The Generics</h1>
+    <div className="container">
+      <div className={`${classes.header} bg-light py-4`}>
+        <h1>The Generics</h1>
+      </div>
     </div>
   );
 };
